3972693d8a63618e72ecb1afd662846908097e3f
Dibo72 <humanida133@gmail.com>
2026-05-08 09:57:24 +0200

    Ejercicio acabado

--- src/main/java/org/example/DireccionComprobacion.java
+++ src/main/java/org/example/DireccionComprobacion.java
@@ -1,17 +1,36 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.awt.*;
+
+public class DireccionComprobacion extends Application {
+    public void start(Stage stage){
+
+        TextField campo = new TextField();
+
+        Label etiqueta = new Label();
+
+        Label informacion = new Label();
+        informacion.setText("Introduzca la direccion:");
+
+        Button boton = new Button("Comprar");
+        boton.setOnAction(e -> {etiqueta.setText("Direccion guardada");});
+
+        VBox root = new VBox(10, informacion, campo, boton, etiqueta);
+
+        Scene scene = new Scene(root, 300, 250);
+        stage.setTitle("Direccion Comprobacion");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch();
     }
 }
